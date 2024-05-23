9297a0e3d3da728508a3aa9431883438b2a49ab1
Mauricio Torres Fernández <torresfernandezmauricio18@gmail.com>
2024-05-23 13:49:33 -0600

    Validación del login

--- src/app/shared/components/login-input/login-input.component.ts
+++ src/app/shared/components/login-input/login-input.component.ts
@@ -6,16 +6,29 @@ import { FormControl } from '@angular/forms';
   templateUrl: './login-input.component.html',
   styleUrls: ['./login-input.component.scss'],
 })
-export class LoginInputComponent  implements OnInit {
-
+export class LoginInputComponent implements OnInit {
   @Input() control!: FormControl;
   @Input() vt_type!: string;
   @Input() vt_label!: string;
   @Input() vt_auto_complete!: string;
   @Input() vt_icon!: string;
+  vb_is_password: boolean;
+  vb_hide: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (this.vt_type == 'password') {
+      this.vb_is_password = true;
+    }
+  }
 
+  mostrar_ocultar_pass(){
+    this.vb_hide = !this.vb_hide;
+    if(this.vb_hide){
+      this.vt_type = 'password';
+    }else{
+      this.vt_type = 'text';
+    }
+  }
 }
