a6f836a47fed10ce9316b3afc386781162f44c3d
Mauricio Torres Fernández <torresfernandezmauricio18@gmail.com>
2024-05-24 17:08:09 -0600

    funcionalidad registro de incidencias 2
    Almacena incidencias según el usuario que esté logueado, almacena imagenes en la base de datos.

--- src/app/shared/components/actualizar-incidencia/actualizar-incidencia.component.ts
+++ src/app/shared/components/actualizar-incidencia/actualizar-incidencia.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { administrator } from 'src/app/models/administrator.model';
 import { Incidencia } from 'src/app/models/incidencia.model';
 import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
@@ -12,33 +11,32 @@ import { UtilsService } from 'src/app/services/utils.service';
   templateUrl: './actualizar-incidencia.component.html',
   styleUrls: ['./actualizar-incidencia.component.scss'],
 })
-
 export class ActualizarIncidenciaComponent implements OnInit {
 
   firebaseService = inject(FirebaseService);
   utilService = inject(UtilsService);
 
-  //usuario
+  // Usuario
   t_usuarios = {} as User;
-  //incidencia
+  // Incidencia
   t_incidencia = {} as Incidencia;
   userId: string | null = null;
 
   form = new FormGroup({
-    cn_id_incidencia: new FormControl('1'),
-    cn_id_usuario: new FormControl(''),
-    cn_id_estado: new FormControl(''),
-    cn_id_afectacion: new FormControl(''),
-    cn_id_riesgo: new FormControl(''),
-    cn_id_prioridad: new FormControl(''),
-    cn_id_categoria: new FormControl(''),
+    cn_id_incidencia: new FormControl(1),
+    cn_id_usuario: new FormControl(null),
+    cn_id_estado: new FormControl(null),
+    cn_id_afectacion: new FormControl(null),
+    cn_id_riesgo: new FormControl(null),
+    cn_id_prioridad: new FormControl(null),
+    cn_id_categoria: new FormControl(null),
     ct_id_img: new FormControl('', [Validators.required]),
     ct_titulo: new FormControl('', [Validators.required]),
     ct_descripcion: new FormControl('', [Validators.required]),
     ct_lugar: new FormControl('', [Validators.required]),
     ct_justificacion_cierre: new FormControl(''),
-    cn_monto: new FormControl(''),
-    cn_numero_incidente: new FormControl(''),
+    cn_monto: new FormControl(null),
+    cn_numero_incidente: new FormControl(null),
   });
 
   ngOnInit() {
@@ -57,59 +55,60 @@ export class ActualizarIncidenciaComponent implements OnInit {
     });
   }
 
-  //obtiene id de usuario logueado
-  
-
   async submit() {
     console.log(this.form.value);
     this.crearIncidencia();
-    //   if (this.form.valid) {
-    //     const loading = await this.utilService.loading();
-    //     await loading.present();
-
-    //     // this.firebaseService
-    //     //   .signIn(this.form.value as User)
-    //     //   .then((resp) => {
-    //     //     this.getUserInfo(resp.user.uid);
-    //     //   })
-    //     //   .catch((error) => {
-    //     //     console.error('Login fallido', error);
-    //     //     this.utilService.presentToast({
-    //     //       message: error.message,
-    //     //       duration: 2500,
-    //     //       color: 'danger',
-    //     //       position: 'bottom',
-    //     //       icon: 'alert-circle-outline'
-    //     //     });
-    //     //   }).finally(() => {
-    //     //     loading.dismiss();
-    //     //   });
-    //   }
   }
 
-  //para crear incidencia
+  // Para crear incidencia
   async crearIncidencia() {
-    let path = `t_incidencias/${this.t_incidencia.uid}/t_incidencias`;
+
+    if (!this.userId) {
+      console.error('El UID del usuario no está definido');
+      this.utilService.presentToast({
+        message: 'Error: El UID del usuario no está definido',
+        duration: 2500,
+        color: 'danger',
+        position: 'bottom',
+        icon: 'alert-circle-outline',
+      });
+      return;
+    }
+
+
+    let path = `t_incidencias/${this.userId}/t_incidencias`;
+    
+    console.log('Path de la incidencia:', path);
 
     const loading = await this.utilService.loading();
     await loading.present();
 
+    let dataUrl = this.form.value.ct_id_img;//valor de la imagen seleccionada
+    let imgPath = `${this.userId}/${Date.now()}`//path unico para la imagen
+    let imgUrl = await this.firebaseService.updateImg(imgPath, dataUrl);
+ 
+    this.form.controls.ct_id_img.setValue(imgUrl);
+    //delete this.form.value.cn_id_incidencia; // Elimina el id y toma el uid creado
+
+    console.log('Datos del formulario antes de agregar:', this.form.value);
+
     this.firebaseService
       .addDocument(path, this.form.value)
       .then(async (resp) => {
+        this.utilService.dismissModal({ success: true });//para cerrar el modal automaticamente
 
-        this.utilService.dismissModal({success: true});
-
+        //mensaje de exito al guardar los datos
         this.utilService.presentToast({
-          message: `Incidencte agregado de manera exitosa`,
+          message: 'Incidencia agregada de manera exitosa',
           duration: 1500,
           color: 'primary',
           position: 'bottom',
           icon: 'checkmark-circle-outline',
         });
       })
+      //mensaje de error al guardar los datos
       .catch((error) => {
-        console.error('Error getting user info:', error);
+        console.error('Error al agregar la incidencia:', error);
         this.utilService.presentToast({
           message: error.message,
           duration: 2500,
@@ -123,11 +122,11 @@ export class ActualizarIncidenciaComponent implements OnInit {
       });
   }
 
-  //para obtener las fotos
+  // Para obtener las fotos
   async takeImage() {
     const dataUrl = (
       await this.utilService.takePicture('Foto de la incidencia')
-    ).dataUrl; //extrae la respuesta que se selecciona
+    ).dataUrl; // Extrae la respuesta que se selecciona
     this.form.controls.ct_id_img.setValue(dataUrl);
   }
 
