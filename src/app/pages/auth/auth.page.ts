582701bcc8fd22303e71f44a8bc5ee57874541cc
Mauricio Torres Fernández <torresfernandezmauricio18@gmail.com>
2024-05-24 00:55:09 -0600

    login funcional 2
    Login funcional recuperando la informacion desde la bd de la persona logueada

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { administrator } from 'src/app/models/administrator.model';
 import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-auth',
@@ -10,25 +13,76 @@ import { FirebaseService } from 'src/app/services/firebase.service';
 })
 export class AuthPage implements OnInit {
   firebaseService = inject(FirebaseService);
+  utilService = inject(UtilsService);
 
   form = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
   });
-  constructor() {}
 
   ngOnInit() {}
 
   async submit() {
     if (this.form.valid) {
+      const loading = await this.utilService.loading();
+      await loading.present();
+
       console.log(this.form.value); // Verifica que aquí se imprimen correctamente los valores
       this.firebaseService
         .signIn(this.form.value as User)
         .then((resp) => {
-          console.log('Login successful', resp);
+          this.getUserInfo(resp.user.uid);
         })
         .catch((error) => {
-          console.error('Login failed', error);
+          console.error('Login fallido', error);
+          this.utilService.presentToast({
+            message: error.message,
+            duration: 2500,
+            color: 'danger',
+            position: 'bottom',
+            icon: 'alert-circle-outline'
+          });
+        }).finally(() => {
+          loading.dismiss();
+        });
+    }
+  }
+
+  async getUserInfo(uid: string) {
+    if (this.form.valid) {
+      const loading = await this.utilService.loading();
+      await loading.present();
+
+      const path = `t_usuarios/${uid}`;
+
+      this.firebaseService.getDocument(path)
+        .then((user: administrator | undefined) => {
+          if (user) {
+            this.utilService.saveLocalStorage('user', user);
+            this.utilService.routerlink('main/home');
+            this.form.reset();
+
+            this.utilService.presentToast({
+              message: `Bienvenido ${user.ct_nombre}`,
+              duration: 1500,
+              color: 'primary',
+              position: 'bottom',
+              icon: 'person-circle-outline'
+            });
+          } else {
+            throw new Error('User data is undefined');
+          }
+        }).catch(error => {
+          console.error('Error getting user info:', error);
+          this.utilService.presentToast({
+            message: error.message,
+            duration: 2500,
+            color: 'danger',
+            position: 'bottom',
+            icon: 'alert-circle-outline'
+          });
+        }).finally(() => {
+          loading.dismiss();
         });
     }
   }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, sendPasswordResetEmail} from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { User } from '../models/user.model';
+import { doc, getDoc, getFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -12,15 +13,21 @@ export class FirebaseService {
   auth = inject(AngularFireAuth);
   firestore = inject(AngularFirestore);
 
-  getAuth(){
-
+  getAuth() {
     return getAuth();
   }
 
-  signIn(user: User){
-
-    return signInWithEmailAndPassword(getAuth(), user.email, user.password);
-
+  signIn(user: User) {
+    return signInWithEmailAndPassword(this.getAuth(), user.email, user.password);
   }
 
+  async getDocument(path: string) {
+    const docRef = doc(getFirestore(), path);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      return docSnap.data();
+    } else {
+      throw new Error('No such document!');
+    }
+  }
 }
