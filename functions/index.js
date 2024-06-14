d1a5987fd08b9137371997cacfa37e5426605bc9
Mauricio Torres Fernández <torresfernandezmauricio18@gmail.com>
2024-06-14 14:36:40 -0600

    Diseño correo
    Diseño del correo enviado a los distintos usuarios.

--- functions/index.js
+++ functions/index.js
@@ -6,8 +6,8 @@ const cors = require('cors')({ origin: true });
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'otrascuentas99@gmail.com', // Reemplaza con tu correo de Gmail
-    pass: 'hpug fhkv nijc ormd', // Reemplaza con la contraseña de aplicación generada
+    user: 'sistemadegestiondeincidencias@gmail.com', // Reemplaza con tu correo de Gmail
+    pass: 'd c w v w vi p m r m g t d p r', // Reemplaza con la contraseña de aplicación generada
   },
 });
 
@@ -25,11 +25,37 @@ exports.sendEmail = functions.https.onRequest((req, res) => {
     }
 
     const mailOptions = {
-      from: 'otrascuentas99@gmail.com', // Reemplaza con tu correo
+      from: 'sistemadegestiondeincidencias@gmail.com', // Reemplaza con tu correo
       to: to,
       subject: subject,
       text: body,
-      html: `<p>${body}</p>`,
+      html: `
+        <div style="background-color: #f3f3f3; padding: 20px;">
+          <table width="600" cellpadding="0" cellspacing="0" border="0" align="center" style="background-color: #ffffff; border: 1px solid #ddd; padding: 20px; font-family: Arial, sans-serif;">
+            <tr>
+              <td style="text-align: center; padding: 10px;">
+                <img src="https://firebasestorage.googleapis.com/v0/b/sicop-is.appspot.com/o/logo%2Flogo_sgi.png?alt=media&token=8b9f0aaa-37f3-48c7-a88c-13fb95e24783" alt="Logo" style="width: 600px;">
+              </td>
+            </tr>
+            <tr>
+              <td style="padding: 20px; text-align: center; font-size: 20px; color: #333;">
+                ${subject}
+              </td>
+            </tr>
+            <tr>
+              <td style="padding: 20px; font-size: 16px; color: #555;">
+                ${body}
+              </td>
+            </tr>
+            <tr>
+              <td style="padding: 20px; text-align: center; color: #aaa; font-size: 12px;">
+                <p>Este es un correo electrónico automático. Por favor, no responda a este mensaje.</p>
+                <p>© 2024 Universidad de Costa Rica</p>
+              </td>
+            </tr>
+          </table>
+        </div>
+      `,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
